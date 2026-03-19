1999c4bcfad3cdc2d99dc850b5d53a7c179c537e
Sahil Garg <me.sahil.gg@gmail.com>
2026-03-19 22:09:46 +0530

    added index page.
    added index page with thumbnail and title.

---
+++ speedtest/app/09_module_a/page.tsx
@@ -0,0 +1,21 @@
+export default function Module() {
+	return (
+		<main className="flex flex-col justify-center items-center gap-10 p-10">
+			<h1 className="text-3xl">09_module_a</h1>
+			<section className="flex justify-center items-center gap-5 flex-wrap">
+				<a href="/09_module_a/A1">
+					<article className="flex flex-col justify-center items-center gap-5 p-5 rounded-2xl border-2">
+						<h2 className="text-2xl">A1</h2>
+						<div className="w-[384px] h-[216px]">
+							<iframe
+								src="/09_module_a/A1"
+								className="w-[1920px] h-[1080px] scale-20 border-2 rounded-4xl origin-top-left"
+								inert
+							></iframe>
+						</div>
+					</article>
+				</a>
+			</section>
+		</main>
+	);
+}
